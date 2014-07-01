25f602adabfc6b5b5d3a3850fb8bb8f5d1f5f357
almazhou <zx_new@126.com>
2014-07-01 22:05:28 +0800

    verify the data in response

--- app/controllers/rproduct_controller.rb
+++ app/controllers/rproduct_controller.rb
@@ -1,6 +1,6 @@
 class RproductController < ApplicationController
 	def index
 		@products = Rproduct.all
-		head 200
+		render json:@products,status:200
 	end
 end
--- spec/controllers/rproduct_controller_spec.rb
+++ spec/controllers/rproduct_controller_spec.rb
@@ -3,9 +3,14 @@ require 'rails_helper'
 RSpec.describe RproductController, :type => :controller do
 	describe "test get method" do
 		it "should get 200 when get successful" do
+			product = Rproduct.create!(:name => "test")
 			get :index
 			expect(response.status).to be(200)
+			json = JSON.parse(response.body)
+			expect(json.length).to be(1)
+			expect(json.at(0)['name']).to eq("test")
 		end
+
 	end
 
 end

e052ec90bf23fb52eb56e887c40fe927a7ec85a2
almazhou <zx_new@126.com>
2014-07-10 21:56:30 +0800

    add 404 test for finding customer failed

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,9 +1,15 @@
 class CustomersController < ApplicationController
+	rescue_from ActiveRecord::RecordNotFound, :with => :not_found
 	def index
 		@customers = Customer.all
 	end
 
 	def show
 		@customer = Customer.find(params[:id]);
+	end
+
+	private
+	def not_found
+		head 404
 	end
 end
--- spec/controllers/customers_controller_spec.rb
+++ spec/controllers/customers_controller_spec.rb
@@ -23,5 +23,10 @@ RSpec.describe CustomersController, :type => :controller do
 			expect(customerJson["name"]).to eq("test");
 			expect(customerJson["uri"]).to match("/customers/" + @customer.id.to_s);
 		end
+
+		it "should return 404 when cannot find specific customer" do
+			get :show, id:10000, format: :json
+			expect(response.status).to be(404);
+		end
 	end 
 end

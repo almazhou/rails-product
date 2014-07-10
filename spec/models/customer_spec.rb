47c15af25d6d9aea67230847afce8604163db187
almazhou <zx_new@126.com>
2014-07-10 22:42:52 +0800

    add customer model test

--- spec/models/customer_spec.rb
+++ spec/models/customer_spec.rb
@@ -1,5 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe Customer, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "validate model customer" do
+  	it "name must not be nil or empty" do
+  		customer = Customer.new name: nil;
+  		expect(customer).to be_invalid;
+  	end
+  end
 end

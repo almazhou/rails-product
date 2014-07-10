7b55eb92ffa5e7598b880454fdf89b5eb153c0e3
almazhou <zx_new@126.com>
2014-07-10 22:45:47 +0800

    add test for not recieving empty name

--- spec/models/customer_spec.rb
+++ spec/models/customer_spec.rb
@@ -2,8 +2,13 @@ require 'rails_helper'
 
 RSpec.describe Customer, :type => :model do
   describe "validate model customer" do
-  	it "name must not be nil or empty" do
+  	it "name must not be nil" do
   		customer = Customer.new name: nil;
+  		expect(customer).to be_invalid;
+  	end
+
+  	it "name must not be empty" do
+  		customer = Customer.new name: "";
   		expect(customer).to be_invalid;
   	end
   end

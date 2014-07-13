fbecdb85cbf41c66a026a43c77b88bc5d1c85fbc
almazhou <zx_new@126.com>
2014-07-13 15:19:36 +0800

    add post test for payment

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -4,4 +4,22 @@ class PaymentsController < ApplicationController
 
 		render json: @payment, status: 200
 	end
+
+	def create
+		param_create = payment_params
+
+		param_create[:customer_id] = params[:customer_id]
+		param_create[:order_id] = params[:order_id]
+
+		@payment_new = Payment.new(payment_params)
+
+		@payment_new.save()
+
+		head 201
+	end
+
+	private 
+	def payment_params
+		params.permit(:customer_id,:order_id,:amount);
+	end
 end
--- spec/controllers/payments_controller_spec.rb
+++ spec/controllers/payments_controller_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe PaymentsController, :type => :controller do
-	describe "test GET" do
+	describe "test GET and POST" do
 		before{
 			@customer = Customer.create!({name:"test"})
 			@order = Order.create!({customer_id: @customer.id, totalCost: 45.0})
@@ -15,6 +15,12 @@ RSpec.describe PaymentsController, :type => :controller do
 			paymentJson = JSON.parse(response.body)
 
 			expect(paymentJson["amount"]).to eq(45.0.to_s)
+		end
+
+		it "should return 201 for post payment of order" do
+			post :create, {customer_id:@customer.id, order_id:@order.id, amount: 45.0}
+
+			expect(response.status).to be(201)
 		end
 	end
 end
